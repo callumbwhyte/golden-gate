74aeba8dee21a7588004ec644e2a4757b02a0cb4
Callum Whyte <hey@callumwhyte.com>
2019-06-05 12:43:20 +0100

    Populating list of old => new Umbraco property type aliases

--- src/Our.Umbraco.GoldenGate.uSync/Helpers/PropertyTypeHelper.cs
+++ src/Our.Umbraco.GoldenGate.uSync/Helpers/PropertyTypeHelper.cs
@@ -1,10 +1,23 @@
 ﻿using System.Collections.Generic;
+using Umbraco.Core;
 
 namespace Our.Umbraco.GoldenGate.uSync.Helpers
 {
     public static class PropertyTypeHelper
     {
-        private static IDictionary<string, string> _aliases = new Dictionary<string, string>();
+        private static IDictionary<string, string> _aliases = new Dictionary<string, string>()
+        {
+            { "Umbraco.ColorPickerAlias", Constants.PropertyEditors.Aliases.ColorPicker },
+            { "Umbraco.ContentPicker2", Constants.PropertyEditors.Aliases.ContentPicker },
+            { "Umbraco.Date", Constants.PropertyEditors.Aliases.DateTime },
+            { "Umbraco.NoEdit", Constants.PropertyEditors.Aliases.Label },
+            { "Umbraco.MediaPicker2", Constants.PropertyEditors.Aliases.MediaPicker },
+            { "Umbraco.MemberPicker2", Constants.PropertyEditors.Aliases.MemberPicker },
+            { "Umbraco.MultiNodeTreePicker2", Constants.PropertyEditors.Aliases.MultiNodeTreePicker },
+            { "Umbraco.RelatedLinks2", Constants.PropertyEditors.Aliases.MultiUrlPicker },
+            { "Umbraco.TinyMCEv3", Constants.PropertyEditors.Aliases.TinyMce },
+            { "Umbraco.TextboxMultiple", Constants.PropertyEditors.Aliases.TextArea }
+        };
 
         public static string GetUpdatedAlias(string oldAlias)
         {
