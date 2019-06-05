03339064e64fb8c72ac95df01ba48241e9476359
Callum Whyte <hey@callumwhyte.com>
2019-06-05 10:14:17 +0100

    Adding Visual Studio solution

---
+++ src/Our.Umbraco.GoldenGate/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Our.Umbraco.GoldenGate")]
+[assembly: AssemblyDescription("a series of extensions, helpers, and libraries designed to smooth over migrating from Umbraco 7 to Umbraco 8")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Callum Whyte")]
+[assembly: AssemblyProduct("Our.Umbraco.GoldenGate")]
+[assembly: AssemblyCopyright("Copyright © 2019 Callum Whyte, and other contributors")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("c228ceb6-5615-42cd-a19e-ee6f8862b26d")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("0.1.0.0")]
+[assembly: AssemblyFileVersion("0.1.0.0")]
