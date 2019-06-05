b0be66fd0fa1a6fabd341fa7f692fba9631197ac
Callum Whyte <hey@callumwhyte.com>
2019-06-05 10:21:26 +0100

    Example IPublishedContent extension method

---
+++ src/Our.Umbraco.GoldenGate/PublishedContentExtensions.cs
@@ -0,0 +1,19 @@
+﻿using Umbraco.Core.Models;
+
+namespace Our.Umbraco.GoldenGate
+{
+    public static class PublishedContentExtensions
+    {
+        public static object Value(this IPublishedContent content, string alias)
+        {
+            var property = content.GetProperty(alias);
+
+            if (property != null)
+            {
+                return property.Value;
+            }
+
+            return null;
+        }
+    }
+}
