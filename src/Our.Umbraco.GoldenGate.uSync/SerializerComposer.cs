636cffce4e0cd52dadea3dbdc856e27f89d705dc
Callum Whyte <hey@callumwhyte.com>
2019-06-05 11:59:23 +0100

    Adding composer to trigger after uSync Core

---
+++ src/Our.Umbraco.GoldenGate.uSync/SerializerComposer.cs
@@ -0,0 +1,14 @@
+﻿using Umbraco.Core.Composing;
+using uSync8.Core;
+
+namespace Our.Umbraco.GoldenGate.uSync
+{
+    [ComposeAfter(typeof(uSyncCoreComposer))]
+    public class SerializerComposer : IUserComposer
+    {
+        public void Compose(Composition composition)
+        {
+
+        }
+    }
+}
